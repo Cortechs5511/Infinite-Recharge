7dbfc20191fbfa8907bf794954efaff4746b3f12
AshishGupta938 <ashvgupta938@gmail.com>
2020-01-08 19:47:56 -0500

    Code for Two Neos
    Small update for code to make it work for two neos.

--- Testing/NeoPID/robot.py
+++ Testing/NeoPID/robot.py
@@ -8,28 +8,31 @@ from rev import CANSparkMax
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
         self.neo0: CANSparkMax = CANSparkMax(1, rev.MotorType.kBrushless)
-        #self.neo1: CANSparkMax = CANSparkMax(11, rev.MotorType.kBrushless)
+        self.neo1: CANSparkMax = CANSparkMax(11, rev.MotorType.kBrushless)
 
-        #neos = wpilib.SpeedControllerGroup(self.neo0, self.neo1)
+        neos = wpilib.SpeedControllerGroup(self.neo0, self.neo1)
+        self.neo1.setInverted(True)
 
         self.neo0.clearFaults()
-        #self.neo1.clearFaults()
+        self.neo1.clearFaults()
 
         self.neo0.setClosedLoopRampRate(3)
         self.neo0.enableVoltageCompensation(10)
-        #self.neo1.setOpenLoopRampRate(5)
+        self.neo1.setClosedLoopRampRate(3)
+        self.neo1.enableVoltageCompensation(10)
         self.revPID = rev._impl.CANPIDController(self.neo0)
         self.revVelPID = rev._impl.CANEncoder(self.neo0)
-        self.revPID.setOutputRange(-0.1, 0.9, 0)
+        self.revPID.setOutputRange(-0.9, 0.9, 0) #lower bound was -0.1 if it breaks
         sd.putNumber("Target RPM", 0)
         sd.putNumber("P", 0.0002)
         sd.putNumber("I", 0)
         sd.putNumber("D", 0.002)
         sd.putNumber("FF", 0.00022)
+        sd.putNumber("Speed", 0)
 
     def teleopInit(self):
         self.neo0.getEncoder().setPosition(0)
-        #self.neo1.getEncoder().setPosition(0)
+        self.neo1.getEncoder().setPosition(0)
 
     def teleopPeriodic(self):
         self.val = sd.getNumber("Target RPM", 0)
@@ -40,6 +43,7 @@ class MyRobot(wpilib.TimedRobot):
         self.revPID.setD(sd.getNumber("D", 0.002), 0)
         self.revPID.setFF(sd.getNumber("FF", 0.00022), 0)
         self.revPID.setReference(self.val, rev.ControlType.kVelocity, 0, 0)
+        #self.neo0.set(sd.getNumber("Speed", 0))
 
 if __name__ == '__main__':
     wpilib.run(MyRobot)
