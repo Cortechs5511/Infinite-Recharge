a6b777d0914800ab09ce51f9540b778f767d1788
AshishGupta938 <ashvgupta938@gmail.com>
2020-01-13 18:50:46 -0500

    Basic Java Command Based
    Java code in Command Based that is super basic and might not actually work on the robot.

---
+++ Testing/JavaCommand01/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,67 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.SetSpeed;
+import frc.robot.subsystems.Drive;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/**
+ * This class is where the bulk of the robot should be declared.  Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+ * (including subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final Drive m_exampleSubsystem = new Drive();
+
+  private final SetSpeed m_autoCommand = new SetSpeed(m_exampleSubsystem);
+
+
+
+  /**
+   * The container for the robot.  Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings.  Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
+   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+  }
+
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return m_autoCommand;
+  }
+
+public void teleopInit(Robot robot) {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (robot.m_autonomousCommand != null) {
+      robot.m_autonomousCommand.cancel();
+    }
+  }
+}
---
+++ Testing/JavaCommand01/src/main/java/frc/robot/subsystems/Drive.java
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drive extends SubsystemBase {
+  //this is actually init
+  public Joystick leftStick = new Joystick(0);
+  public Joystick rightStick = new Joystick(1);
+  public CANSparkMax left0 = new CANSparkMax(10, MotorType.kBrushless);
+  public CANSparkMax left1 = new CANSparkMax(11, MotorType.kBrushless);
+  public SpeedControllerGroup left = new SpeedControllerGroup(left0, left1);
+  public CANSparkMax right0 = new CANSparkMax(20, MotorType.kBrushless);
+  public CANSparkMax right1 = new CANSparkMax(21, MotorType.kBrushless);
+  public SpeedControllerGroup right = new SpeedControllerGroup(right0, right1);
+  
+  public Drive() {
+    //basically not init
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
