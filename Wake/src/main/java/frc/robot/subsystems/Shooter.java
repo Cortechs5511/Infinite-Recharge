9c022a92c65ac100972be40c06f1526521f688b0
Andrew Wang <aywang71@gmail.com>
2020-02-08 12:48:05 -0500

    One line change - prereq to pull

--- Wake/src/main/java/frc/robot/subsystems/Shooter.java
+++ Wake/src/main/java/frc/robot/subsystems/Shooter.java
@@ -9,6 +9,7 @@ import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
@@ -63,5 +64,6 @@ public Shooter() {
 
   @Override
   public void periodic() {
+    
   }
 }
