69ab2fec426988315f505b44aff3e293b7ed55d2
ericliu0000 <ericliu1266@gmail.com>
2020-02-08 16:24:11 -0500

    2/8 auto work
    (Eric, Jacob) Implementation of bobtrajectory ito auto

---
+++ Wake/src/main/java/frc/robot/commands/auto/r/SimpleTower.java
@@ -0,0 +1,27 @@
+package frc.robot.commands.auto.r;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class SimpleTower extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+
+  public SimpleTower() {
+  }
+
+  @Override
+  public void initialize() {
+  }
+
+  @Override
+  public void execute() {
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
