d0f7796288ef1ce730e98aed2a75b59559ae865d
Andrew Wang <aywang71@gmail.com>
2020-02-23 15:40:10 -0500

    Update DataRecorder.java

--- Wake/src/main/java/frc/robot/commands/DataRecorder.java
+++ Wake/src/main/java/frc/robot/commands/DataRecorder.java
@@ -3,30 +3,34 @@ package frc.robot.commands;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drive;
 
 public class DataRecorder extends CommandBase {
-    private static final int MAX_ROWS = 9000;
+    private static final int MAX_ROWS = 50*15;
     private static final int COLUMNS = 8;
     private Drive m_drive;
     public double[][] data = new double[MAX_ROWS][COLUMNS];
     private int row = 0;
     private int col = 0;
-    private double time = 0;
-    private boolean start = false;
+    private boolean check = false;
+    private Timer time = new edu.wpi.first.wpilibj.Timer();
     public DataRecorder(Drive drive) {
     }
     @Override
     public void initialize(){
-        start = false;
+        check = false;
+        time.reset();
+        time.start();
     }
     @Override
     public void execute() {
         if (m_drive.getLeftOutput.get() != 0 || m_drive.getRightOutput.get() != 0) {
-            start = true;
+            check = true;
         }
-        col = 0;
+        if (check == true){
+            col = 0;
         data[row][col++] = m_drive.getLeftVelocity.get();
         data[row][col++] = m_drive.getRightVelocity.get();
         //data[row][col++] = m_drive.getLeftPosition.get();
@@ -34,15 +38,14 @@ public class DataRecorder extends CommandBase {
         data[row][col++] = m_drive.getLeftOutput.get();
         data[row][col++] = m_drive.getRightOutput.get();
         data[row][col++] = m_drive.getGyroAngle.get();
-        data[row][col++] = time;
-
+        data[row][col++] = time.get();
         row++;
-        time += 0.02;
+        }  
     }
 
     @Override
     public boolean isFinished() {
-        if (time >= 170) {
+        if (row >= MAX_ROWS) {
             return true;
         } else {
             return false;
