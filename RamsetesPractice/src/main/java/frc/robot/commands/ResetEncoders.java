5c12333dc5e7d712e5a8ff52618139862eda324c
ProgramminSmarts <hsbosskid@gmail.com>
2020-03-11 19:37:02 -0400

    Added reset encoders command

---
+++ RamsetesPractice/src/main/java/frc/robot/commands/ResetEncoders.java
@@ -0,0 +1,34 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drive;
+
+public class ResetEncoders extends CommandBase {
+
+	private Drive m_drive;
+
+    public ResetEncoders(Drive drive) {
+        m_drive = drive;
+	}
+
+	@Override
+	public void initialize() {
+        m_drive.resetRightEnc();
+        m_drive.resetLeftEnc();
+        
+	}
+
+	@Override
+	public void execute() {
+	}
+
+	@Override
+	public boolean isFinished() {
+        return true;
+	}
+
+	@Override
+	public void end(boolean interrupted) {
+		 
+	}
+}
